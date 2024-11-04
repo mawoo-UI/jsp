fa0a0b76926f0992b5bc024617c8e3125c06dee6
mawoo <hanjutun@gmail.com>
2024-11-05 08:58:14 +0900

    2024-11-04수업

--- member_post/src/main/java/servlet/member/Signup.java
+++ member_post/src/main/java/servlet/member/Signup.java
@@ -8,8 +8,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import service.MemberService;
+import service.MemberServiceImpl;
+import vo.Member;
+
+
 @WebServlet("/signup")
 public class Signup extends HttpServlet{
+	private MemberService service = new MemberServiceImpl();
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -18,6 +24,32 @@ public class Signup extends HttpServlet{
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.setCharacterEncoding("utf-8");
+		String id = req.getParameter("id");
+		String pw = req.getParameter("pw");
+		String name = req.getParameter("name");
+		String email = req.getParameter("email");
+		String roadAddr = req.getParameter("roadAddr");
+		String detailAddr = req.getParameter("detailAddr");
+		
+//		Member member = new Member();
+//		member.setId(id);
+//		기본값으로 되어있으면 한글깨짐 //한글 3byte ,영문 1byte(?)
+//		iso 8859-1 (기본값)
+		Member member = Member.builder()
+				.id(id)
+				.pw(pw)
+				.name(name)
+				.email(email)
+				.roadAddr(roadAddr)
+				.detailAddr(detailAddr)
+				.build();
+		
+		System.out.println(member);
+		
+		service.register(member);
+		
+		resp.sendRedirect("signup");
 	}
 	
 }
--- member_post/src/main/java/vo/Member.java
+++ member_post/src/main/java/vo/Member.java
@@ -13,4 +13,76 @@ public class Member {
 	private String roadAddr;
 	private String detailAddr;
 	private Date regdate;
+	
+	public  Member() {
+		
+	}
+
+	
+//	private Member(String id,String pw,String name,String email,String roadAddr,String detailAddr,Date regdate) {
+//		
+//	}
+	
+	
+	public static M builder() {
+		return new M();
+	}
+	private Member(String id, String pw, String name, String email, String roadAddr, String detailAddr, Date regdate) {
+		super();
+		this.id = id;
+		this.pw = pw;
+		this.name = name;
+		this.email = email;
+		this.roadAddr = roadAddr;
+		this.detailAddr = detailAddr;
+		this.regdate = regdate;
+	}
+//	
+	public static class M {
+		String id;	
+		String pw;
+		String name;
+		String email;
+		String roadAddr;
+		String detailAddr;
+		Date regdate;
+		
+		public M id(String id) {
+			this.id = id;
+			return this;
+		}
+		
+		public M pw(String pw) {
+			this.pw = pw;
+			return this;
+		}
+		public M name(String name) {
+			this.name = name;
+			return this;
+		}
+		public M email(String email) {
+			this.email = email;
+			return this;
+		}
+		
+		public M roadAddr(String roadAddr) {
+			this.roadAddr = roadAddr;
+			return this;
+		}
+		
+		public M detailAddr(String detailAddr) {
+			this.detailAddr = detailAddr;
+			return this;
+		}
+		public M regdate(Date regdate) {
+			this.regdate = regdate;
+			return this;
+		}
+		
+		
+		public Member build() {
+			return new Member(id,pw,name, email, roadAddr, detailAddr ,regdate);
+		}
+		
+	}
 }
